a5c324c00a8f41d49d5d0f9eaa89f2ec1ea9b3e8
Andrea Brion <860595@stud.unive.it>
2021-10-23 16:44:19 +0200

    Vogliamo definire un gara fra veicoli
    Creazione metodo race, vediamo come funzionano i vari override di accellerate

--- src/vehicles/Race.java
+++ src/vehicles/Race.java
@@ -4,33 +4,43 @@ import vehicles.fuel.FuelTank;
 import vehicles.fuel.FuelType;
 
     public class Race {
-    public static int race (Vechicle v1, Vechicle v2) {
-        return 1;
+
+        /**
+         *
+          * @param v1 first vehicle
+         * @param v2 second vehicle
+         * @param length
+         * @return the id of the winner or "-1" if there is a draw
+         */
+    public static int race (Vechicle v1, Vechicle v2, double length) {
+        //qui viene invocato la versione di accellerate corretta sulla base della
+        //classe di v1 e v2, se v1 è una bici viene usato accellerate di Bicycle!
+        v1.fullBrake();
+        v2.fullBrake();
+        double distanceV1 = 0, distanceV2 = 0;
+        while ((distanceV1 < length) && (distanceV2 < length)) {
+            v1.accellerate(Math.random() * 10);
+            v2.accellerate(Math.random() * 10);
+            distanceV1 += v1.getSpeed();
+            distanceV2 += v2.getSpeed();
+        }
+        if (distanceV1 >= length){
+            if (distanceV2 >= length)
+                return -1;
+            else
+                return 1;
+        }
+        else
+            return 2;
     }
     public static void main(String[] args) {
-        FuelType petrol = new  FuelType("Diesel", 0.02, 1.8);
         FuelType diesel = new FuelType("Diesel", 0.01, 1.4);
-        Car myCar = new Car (new FuelType("diesel", 1.4, 0.01));
         FuelTank two_lt = new FuelTank(diesel, 2);
-        FuelTank three_lt = new FuelTank(diesel, 3);
-        diesel.setCostPerLiter(1.35);
-        myCar.refuel(two_lt);
-        double increase = 100;
-        myCar.accellerate(increase);
-        Car yourCar = new Car(diesel);
-        yourCar.refuel(three_lt);
-        yourCar = myCar;
-        myCar.brake(30);
-        yourCar.accellerate(50);
-        myCar.brake(200);
-        Bicycle B = new Bicycle(0, 1 ,1);
-        race(myCar, B); // anche se questo metodo non fa nulla comunque possiamo passare qualsiasi veicolo!
+        Car C = new Car (new FuelType("diesel", 1.4, 0.01));
+        Bicycle B = new Bicycle(10, 1 ,1);
         Truck T = new Truck(diesel);
-        myCar.accellerate(9999999);
-        boolean result = myCar.isFuelEmpty();
-        result = yourCar.isFuelEmpty();
-        //T avrà i metodi di car, i propri e quelli di vehicle, come B avrà i metodi della superclasse (vehicle) più i propri (quelli di bicycle -> posti in grassetto nel menù a tendina)
-
-
+        C.refuel(two_lt);
+        T.refuel(two_lt);
+        race(B,C, 100);
     }
 }
