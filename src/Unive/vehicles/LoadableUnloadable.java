2fa88ceeaf7647587e2d7af4c700ed33e6fde6b3
Andrea Brion <860595@stud.unive.it>
2021-10-30 16:33:59 +0200

    Update

--- src/Unive/vehicles/LoadableUnloadable.java
+++ src/Unive/vehicles/LoadableUnloadable.java
@@ -1,7 +1,8 @@
 package Unive.vehicles;
 
-public interface LoadableUnloadable extends Loadable,Printable {double loadedCharge = 0;
-     public default double unload() {
+public interface LoadableUnloadable extends Loadable,Printable {
+    double loadedCharge = 0;
+    public default double unload() {
         double value = this.getLoad(); //vede getLoad e setLoad in quanto sottoclasse!
         this.setLoad(0);                //così come vede il metodo chargeLoad
         return value;
