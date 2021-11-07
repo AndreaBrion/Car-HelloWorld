91975cbc297e43d0416fae1cfb6d2a674d177d59
Andrea Brion <860595@stud.unive.it>
2021-11-07 17:55:26 +0100

    Quarta parte - Array come liste?
    -Creo un array di Bicycle e Vechicle
    -Covarianza degli array (vedi debugger)
    Java per supportare la covarianza aggiunge dei controlli durante l'esecuzione -> unico caso in java di covarianza

--- src/Unive/util/List.java
+++ src/Unive/util/List.java
@@ -2,6 +2,8 @@ package Unive.util;
 
 import Unive.vehicles.Bicycle;
 import Unive.vehicles.Vechicle;
+import Unive.vehicles.autovehicles.Car;
+import Unive.vehicles.fuel.FuelType;
 
 import javax.sound.sampled.BooleanControl;
 import java.util.Arrays;
@@ -24,10 +26,10 @@ public class List<V> {
         return elements[i];
     }
     public static void main(String[] args) {
-        List<Vechicle> listV = new List<Vechicle>();
+       /* List<Vechicle> listV = new List<Vechicle>();
         List<Bicycle> listB = new List<Bicycle>();
         List<Vechicle> listV2 = new List<Vechicle>();
-       /* listV2 = listB; Qui otteniamo un errore durante compilazione
+        listV2 = listB; Qui otteniamo un errore durante compilazione
        Immaginiamo possa assegnare una lista di Bicycle ad una lista di Vehicle.
           Bicycle b = listB.get(0); Su una lista di veicoli posso fare il put() di un qualsiasi veicolo, in quanto :
           listB.add(new Bicycle(0)); la lista è parametrizzata sul tipo V, il tipo V è Vechicle,
@@ -43,7 +45,32 @@ public class List<V> {
           Bicycle b1 = listB.get(0);
           Se la mia lista di bici è una lista di Vechicle posso aggiungere un sottotipo di Vechicle
           alla lista -> ERRORE
-          
+*/
+          /*
+          Se avessimo lo stesso approccio non potremmo assegnare un ad un array di Vechicle uno di Bicycle, qui però funziona
+          Non possiamo fare il contrario
+          */
+          Vechicle[] v = new Vechicle[1];
+          Bicycle[] b = new Bicycle[1];
+          v[0] = new Car(0, new FuelType("Petrol",1.4, 0.01));
+          b[0] = new Bicycle(0);
+          //Infatti posso fare :
+          v = b;
+          /* ma non posso fare :
+          b = v;
+          L'idea qui è il concetto di covarianza
+          Gli array in Java sono COVARIANTI, cioè ad un array di tipo A posso assegnare un array
+          di tipo N sse il tipo statico di N è sottotipo del valore assegnato (cioè di A)
+          Vediamo alcuni problemi dato ciò :
+           */
+          v = b;
+          v[0] = new Car(0, new FuelType("Petrol",1.4, 0.01)); // qui otterrei ERRORE
+          Bicycle b1 = b[0]; // qui so che sto estraendo una Bicycle
+        /*
+        L'errore indica che non possiamo porre una Car in v[0], in quanto v[] è un array di Bicycle dove il tipo statico è Vechicle mentre
+        il dinamico di Bicycle
+        L'errore sta nel voler porre un elemento Car in un array di Bicycle!
+          * */
     }
-        */
+
 }
