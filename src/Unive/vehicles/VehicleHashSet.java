d797e569ea665b1cf3f26ad4b628bd3251772fef
Andrea Brion <860595@stud.unive.it>
2021-12-08 18:47:24 +0100

    Vediamo un Esercizio update
    Data un'istanza di Class vogliamo andare ad un'istanza dell'oggetto rappresentato da quella classe Class : voglio creare un vehicleHashset con tutti i veicoli
    Poi per ciascuna classe c che ho in AllVehicles voglio aggiungere che data un'istanza di classe c mi ritorna un istanza del veicolo corrispondente
    Vogliamo che dato un parametro disponiamo di un valore da porci -> se riusciamo a costruire un array di oggetti (uno per parametro del tipo giusto) allora
     possiamo invocare il costruttore, avere un'istanza ed aggiungerla alla nostra collezione
    
    All'interno dell'esempio abbiamo usato le collection (nel momento in cui volevamo un numero arbitrario di veicoli ci serve
    la collection -> più semplice aggiungere veicoli di un array), abbiamo usato la reflection per vedere
    le istanze di classe che ci rappresentano i veicoli (ossia i .class)
    Nel createVehicle utilizzavamo for each loop per poter iterare sulle collection, usato le reflection e il cast dinamico
    Nel getInstanceOf abbiamo usato la classe Object in quanto ci serviva rappresentare un valore di cui non sapevamo cosa fosse,
    usiamo il boxing e unboxing, usiamo metodi della classe Object
    Infine in HashSet eccezioni, collection, generics e for each loop
    Questo che abbiamo visto è il primo esempio dove mischiamo i concetti visti

--- src/Unive/vehicles/VehicleHashSet.java
+++ src/Unive/vehicles/VehicleHashSet.java
@@ -6,6 +6,10 @@ import Unive.vehicles.autovehicles.Truck;
 import Unive.vehicles.fuel.FuelTank;
 import Unive.vehicles.fuel.FuelType;
 
+import javax.naming.event.ObjectChangeListener;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
 import java.nio.channels.ByteChannel;
 import java.util.Collection;
 import java.util.HashMap;
@@ -37,13 +41,6 @@ public class VehicleHashSet<T extends Vechicle> extends HashSet<T> { //qui ho fi
         }
     }
 
-    public static void main (String[] args) throws ImpossibleAccellerateException {
-        Collection<Class> allVehicles = getAllVechiclesClasses();
-        for(Class c : allVehicles) {
-            System.out.println(c.getName());
-        }
-
-    }
     static private Collection<Class> getAllVechiclesClasses() {
         HashSet<Class> result = new HashSet<>();
         result.add(Bicycle.class);
@@ -52,4 +49,71 @@ public class VehicleHashSet<T extends Vechicle> extends HashSet<T> { //qui ho fi
         result.add(Carretto.class);
         return result;
     }
+
+    private static Vechicle createInstanceOf(Class c) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+external:for (Constructor constructor : c.getConstructors()) {
+            /*constructor.setAccessible(true);
+            Così posso creare l'istanza. Qui non so che veicoli stiamo creando!
+            indico : dammi i costruttori, quando trovo quello adatto lo uso!
+            */
+            Parameter[] parameters = constructor.getParameters(); //Con getParameters prendiamo tutti i parametri che richiede e magari costruirceli
+            Object[] parametersValues = new Object[parameters.length];  // Se ho n parametri devo avere n valori da passare al costruttore
+            for (int i = 0; i < parameters.length; i++) {
+                Object o = getConcreteValue(parameters[i].getType()); //nell'oggetto o pongo un'istanza della classe object che
+                /*rappresenti un valore possibile per quel parametro dell'iesimo parametro
+                NON CI INTERESSANO I VALORI DEI PARAMETRI, CI INTERESSA COSTRUIRE UN'ISTANZA DI QUELLA CLASSE PER POI CREARE LA GARA
+                */
+                if (o != null)
+                    parametersValues[i] = o;
+                else
+                    continue external; //indico di continuare il for più esterno
+            }
+            return (Vechicle) constructor.newInstance(parametersValues);
+        }
+        return null; // non sono  stato in grado di istanziare il veicolo
+    }
+
+    private static Object getConcreteValue(Class<?> type) {
+        if (type.equals(FuelType.class))
+            return new FuelType("diesel", 0.015, 0.01);
+        if (type.equals(double.class) || type.equals(Double.class))
+            return 0.0; //esplicitamente viene eseguiti return Double.valueOf(0.0);
+        if (type.equals(int.class) || type.equals(Integer.class))
+            return Integer.valueOf(0);
+
+        return null;
+
+    }
+
+    private static void refuel(VehicleHashSet<Vechicle> allInstatiatedVehicles) {
+        for (Vechicle v : allInstatiatedVehicles) {
+            if (v instanceof Car)
+                ((Car) v).refuel(500);
+        }
+    }
+
+    public static void main(String[] args) throws ImpossibleAccellerateException, IllegalAccessException,InstantiationException, InvocationTargetException {
+        Collection<Class> allVehicles = getAllVechiclesClasses();
+        for (Class c : allVehicles) {
+            System.out.println(c.getName());
+        }
+        VehicleHashSet<Vechicle> allInstatiatedVehicles = new VehicleHashSet();
+        for (Class c : allVehicles) {
+            Vechicle v = createInstanceOf(c);
+            if (v != null)
+                allInstatiatedVehicles.add(createInstanceOf(c));
+            else
+                System.err.println("Unable to instantiate Vehicle " + c.getName());
+        }
+        for (Vechicle v : allInstatiatedVehicles) {
+            System.out.println(v);
+        }
+        refuel(allInstatiatedVehicles);
+        System.out.println("The winner is "+allInstatiatedVehicles.new_race(100.0)); //dobbiamo fare benzina!
+    }
+
+
 }
+
+
+
